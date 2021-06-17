2d7a6f14567f66682b39fede15088d801c4193a0
Marcin Kubiak <hugo26@o2.pl>
2021-06-17 13:13:27 +0200

    create app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import { Grid } from '@material-ui/core';
 
-function App() {
-  return <div className="App">learn</div>;
+interface Props {
+  children: Required<ReactNode>;
+}
+
+function App(props: Props) {
+  return (
+    <Grid container role="appcontainer">
+      {props.children}
+    </Grid>
+  );
 }
 
 export default App;
