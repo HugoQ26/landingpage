045f9653158252fb7aee86b26e838612bf4fa328
Marcin Kubiak <hugo26@o2.pl>
2021-06-17 13:40:17 +0200

    add landing page placholder

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LandingPage from './pages/landingPage/LandingPage';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App>todo</App>
+      <App>
+        <Router>
+          <Switch>
+            <Route path="/">
+              <LandingPage />
+            </Route>
+          </Switch>
+        </Router>
+      </App>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
---
+++ src/pages/landingPage/LandingPage.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LandingPage from './LandingPage';
+
+import { render, screen } from '@testing-library/react';
+
+describe('Landing page component', () => {
+  render(<LandingPage />);
+  it('should render', () => {
+    expect(screen.getByText('Landing page')).toBeTruthy();
+  });
+});
