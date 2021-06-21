b33f17ed89512e7e1b447e72d3f518e92fb2ec6b
Marcin Kubiak <hugo26@o2.pl>
2021-06-21 18:12:24 +0200

    functions

--- src/utils/getWeather.ts
+++ src/utils/getWeather.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import { getPreciseLocation } from './getLocation';
 
+const weatherApiKey = '4502f50f8d3d6a31afd458e4577e01b4';
+
 export const getWeatherApi = async () => {
   try {
     const data = await getPreciseLocation();
     if (Array.isArray(data)) {
       const [lat, long] = data;
-      const apiCall = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&units=metric&lon=${long}&lang=pl&appid=${process.env.WEATHER}`;
+      const apiCall = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&units=metric&lon=${long}&lang=pl&appid=${weatherApiKey}`;
       console.log(apiCall);
       const weather = await axios.get(apiCall);
 
